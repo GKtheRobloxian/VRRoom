767063700fe52bb08f3a7747448dc550734e1e42
Samuel Lanzuela <amlanzuela@gmail.com>
2024-02-01 13:23:14 -0800

    So that just didn't work.

--- LanzuelaRoom/Assets/TranslateTrails.cs
+++ LanzuelaRoom/Assets/TranslateTrails.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TranslateTrails : MonoBehaviour
 {
     GameObject[] trails;
+    GameObject[] trailSaved;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +15,24 @@ public class TranslateTrails : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        trails = GameObject.FindGameObjectsWithTag("Trail");
+        trailSaved = GameObject.FindGameObjectsWithTag("Saved Trail");
+    }
+
+    public void TeleportTrails()
+    {
+        foreach (GameObject trails in trailSaved)
+        {
+            Destroy(trails);
+        }
+
         foreach (GameObject trail in trails)
         {
             trail.transform.Translate(Vector3.right * 1.5f + Vector3.back * 0.5f);
             trail.transform.SetParent(GameObject.Find("Trail Parent").transform);
+            trail.transform.localScale /= 4f;
+            trail.transform.localPosition /= 4f;
+            trail.gameObject.tag = "Saved Trail";
         }
     }
 }
